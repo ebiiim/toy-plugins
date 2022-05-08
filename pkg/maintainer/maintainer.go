78185ad3ada452f995ff27d3b187c155fefba066
ebiiim <mail@ebiiim.com>
2022-05-08 23:45:15 +0900

    feat: maintainer.enabled

--- pkg/maintainer/maintainer.go
+++ pkg/maintainer/maintainer.go
@@ -3,6 +3,7 @@ package maintainer
 import (
 	"context"
 	"net/mail"
+	"strings"
 
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -17,9 +18,10 @@ var _ framework.FilterPlugin = &Maintainer{}
 
 const (
 	Name                        = "Maintainer"
-	annotationKey               = "ebiiim.com/maintainer"
-	ErrReasonAnnotationNotFound = "\"" + annotationKey + "\" didn't found in Pod's Annotations"
-	ErrReasonInvalidAddress     = "\"" + annotationKey + "\" in Pod's Annotations didn't match RFC 5322 (e.g. \"John Smith <john@example.com>\")"
+	annotKeyAddr                = "ebiiim.com/maintainer.address"
+	annotKeyEnabled             = "ebiiim.com/maintainer.enabled"
+	ErrReasonAnnotationNotFound = "\"" + annotKeyAddr + "\" didn't found in Pod's Annotations"
+	ErrReasonInvalidAddress     = "\"" + annotKeyEnabled + "\" in Pod's Annotations didn't match RFC 5322 (e.g. \"John Smith <john@example.com>\")"
 )
 
 func (m *Maintainer) Name() string {
@@ -27,11 +29,17 @@ func (m *Maintainer) Name() string {
 }
 
 func (m *Maintainer) Filter(ctx context.Context, state *framework.CycleState, pod *v1.Pod, nodeInfo *framework.NodeInfo) *framework.Status {
-	mnt, ok := pod.Annotations[annotationKey]
+	// skip this filter if not enabled
+	en, ok := pod.Annotations[annotKeyEnabled]
+	if !ok || strings.ToLower(en) != "true" {
+		return nil
+	}
+	// validate the address
+	addr, ok := pod.Annotations[annotKeyAddr]
 	if !ok {
 		return framework.NewStatus(framework.UnschedulableAndUnresolvable, ErrReasonAnnotationNotFound)
 	}
-	if _, err := mail.ParseAddress(mnt); err != nil {
+	if _, err := mail.ParseAddress(addr); err != nil {
 		return framework.NewStatus(framework.UnschedulableAndUnresolvable, ErrReasonInvalidAddress)
 	}
 	return nil
